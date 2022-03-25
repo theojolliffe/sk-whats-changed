b2baa395b74a532224fcc510fe0693631a2d888b
Theo Jolliffe <theojolliffe@gmail.com>
2022-03-25 15:41:23 +0000

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+/** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+
+const production = process.env.NODE_ENV === 'production';
+
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter({
+			// Options below are defaults
+			pages: 'build',
+			assets: 'build'
+		}),
+		prerender: {
+			enabled: production
+		},
+		paths: {
+			assets: production ? 'http://theojolliffe.github.io/sk-whats-changed' : '',
+			base: production ? '/sk-whats-changed' : ''
+		}
+	}
+};
+
+export default config;
